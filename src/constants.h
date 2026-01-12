01ccf83c015986696999fce797e1f5aa4dfb8843
Blake Abernathy <minerabacuss@gmail.com>
2026-01-12 16:06:00 -0600

    Corrected light phase count

--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,7 @@ constexpr int CHUNK_SIZE = 32;
 constexpr int WORLD_HEIGHT_LIMIT = 128;
 constexpr int GROUND_LEVEL = 64;
 constexpr int BLOCKS_IN_CHUNK = (CHUNK_SIZE + 2) * (CHUNK_SIZE + 2) * WORLD_HEIGHT_LIMIT;
-constexpr int LIGHT_PHASES = 10;
+constexpr int LIGHT_PHASES = 11;
 constexpr int SECONDS_PER_LIGHT_PHASE = 30;
 constexpr float FIXED_DELTA_TIME = 0.02f;
 
