7671f1df70a1a5cf36f8de6e0e8181c12394aadc
Blake Abernathy <minerabacuss@gmail.com>
2026-01-29 16:35:18 -0600

    Updated comment

--- src/moon.cpp
+++ src/moon.cpp
@@ -37,8 +37,8 @@ Moon::Moon(int moon_id, MoonSettings moon_settings)
         moon_data_file.close();
     }
 
-    // Choose random fog color (this code was reverse engineered
-    // from an unknown version of the original Lunacraft)
+    // Choose random fog color (this code was reverse engineered from an
+    // unknown version of the original game; definitely at least v1.91)
 
     RNG rng;
 
